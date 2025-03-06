8d0bba37a0203dd1521405e3bf69f2b7989ab156
IsaRB <romerobellorini@gmail.com>
2025-03-06 22:24:32 +0100

    Add new changes Ejercicio4

--- src/main/java/Ejercicios/Ejercicio4.java
+++ src/main/java/Ejercicios/Ejercicio4.java
@@ -4,10 +4,13 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Ejercicio4 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
+    private Scanner scanner;
 
+    public Ejercicio4(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void ejecutar() {
         System.out.print("Introduce la cantidad de números aleatorios a generar: ");
         int cantidad = scanner.nextInt();
 
@@ -16,16 +19,19 @@ public class Ejercicio4 {
 
         System.out.print("Introduce el valor máximo del rango: ");
         int max = scanner.nextInt();
-        scanner.close();
+
+        // No cerramos el scanner para evitar problemas en Main
 
         System.out.println("Números aleatorios generados:");
         for (int i = 0; i < cantidad; i++) {
-            System.out.println(generarNumeroAleatorio(min, max, random));
+            System.out.println(generarNumeroAleatorio(min, max));
         }
     }
 
-    public static int generarNumeroAleatorio(int min, int max, Random random) {
+    public static int generarNumeroAleatorio(int min, int max) {
+        Random random = new Random(); // Se crea dentro del método
         return random.nextInt((max - min) + 1) + min;
     }
 }
+
 
